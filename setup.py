d78a311f2c96388b30d12eacb22ff31957808f85
Nix Li <xinli1997@gmail.com>
2017-11-30 23:10:39 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+os.system('sudo apt-get update -y')
+os.system('sudo apt-get install python-pip -y')
+os.system('sudo pip install bottle')
+os.system('sudo pip install oauth2client')
+os.system('sudo pip install --upgrade google-api-python-client')
+os.system('sudo pip install beaker')
+os.system('sudo pip install pyparsing')
