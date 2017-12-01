66217d56d176b7cc4624ed138511b8514a6ede62
Nix Li <xinli1997@gmail.com>
2017-11-30 23:13:25 -0500

    Made to rin 0.0.0.0 80

--- srv.py
+++ srv.py
@@ -279,4 +279,4 @@ def errorHandler(error):
 #def errorHandler(error):
 #     return  template('otherError.tpl')
 
-run(host='localhost', port = 8080,  debug=True, reloader=True, app=app)
+run(host='0.0.0.0', port = 80,  debug=True, reloader=True, app=app)
