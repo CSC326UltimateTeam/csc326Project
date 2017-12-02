ff6486d21f516067149e57f6d53f977e108d3bad
Nix Li <xinli1997@gmail.com>
2017-12-02 03:12:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 os.system('sudo apt-get update -y')
 os.system('sudo apt-get install python-pip -y')
-os.system('sudo pip install bottle')
+os.system('sudo pip install bottle beautifulsoup')
 os.system('sudo pip install oauth2client')
 os.system('sudo pip install --upgrade google-api-python-client')
 os.system('sudo pip install beaker')
