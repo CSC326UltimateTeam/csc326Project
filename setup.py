36450946d09e8569ccf6feddb61632f1b907c632
Nix Li <xinli1997@gmail.com>
2017-12-02 22:54:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,8 @@ os.system('sudo pip install oauth2client')
 os.system('sudo pip install --upgrade google-api-python-client')
 os.system('sudo pip install beaker')
 os.system('sudo pip install pyparsing')
+os.system('sudo pip install filedepot')
+os.system('sudo pip install selenium')
+os.system('sudo apt-get install phantomjs -y')
+os.system('ssudo pip install tensorflow --no-cache-dir')
 
