10b53af1f96fc20b0555469f9b1d6f78f3632149
Nix Li <xinli1997@gmail.com>
2017-12-03 01:18:42 -0500

    Update server_initiate.py

--- server_initiate.py
+++ server_initiate.py
@@ -41,7 +41,7 @@ def deployment_aws():
     #create a sucurity group called csc326-group5, as specified in the handout
     connection.delete_security_group('csc326group5')
     csc_security_group= connection.create_security_group(name ='csc326group5', description='used for csc326 lab')
-
+    
     #authorize some access of this group, so we could ssh and browes the instance later on
     csc_security_group.authorize(ip_protocol='ICMP', from_port=-1, to_port=-1, cidr_ip='0.0.0.0/0')
     csc_security_group.authorize(ip_protocol='TCP', from_port=22, to_port=22, cidr_ip='0.0.0.0/0')
@@ -52,9 +52,9 @@ def deployment_aws():
 
     print("creating instance...")
     #create the instance with the parameters we set up, and some addtional options
-    aws_instance = connection.run_instances('ami-68244112', min_count=1, max_count=1,
+    aws_instance = connection.run_instances('ami-d0ba0cb8', min_count=1, max_count=1,
                   key_name='group5_permission', security_groups=['csc326group5'],
-                  addressing_type=None, instance_type='t2.micro', placement=None,
+                  addressing_type=None, instance_type='m1.small', placement=None,
                   kernel_id=None, ramdisk_id=None, monitoring_enabled=True, subnet_id=None,
                   block_device_map=None, disable_api_termination=False, instance_initiated_shutdown_behavior=None,
                   private_ip_address=None, placement_group=None, client_token=None, security_group_ids=[csc_security_group.id],
@@ -68,7 +68,7 @@ def deployment_aws():
         time.sleep(2)
         running_instance.update()
     print("\nDone!")
-
+    time.sleep(1)
     print("setting up server on instance, this may take a while...")
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
