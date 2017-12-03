f4ab5bfb3327696e4121b13fb0cca452dd1e9de4
Nix Li <xinli1997@gmail.com>
2017-12-03 00:52:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,4 @@ os.system('sudo pip install pyparsing')
 os.system('sudo pip install filedepot')
 os.system('sudo pip install selenium')
 os.system('sudo apt-get install phantomjs -y')
-<<<<<<< HEAD
-os.system('ssudo pip install tensorflow --no-cache-dir')
-=======
 os.system('sudo pip install tensorflow --no-cache-dir --ignore-installed')
-
->>>>>>> origin/master
