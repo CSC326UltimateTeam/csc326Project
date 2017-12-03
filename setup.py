3bcddea9bd20406d4b15f2c90d3bd2625bb0280f
Nix Li <xinli1997@gmail.com>
2017-12-03 00:03:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 os.system('sudo apt-get update -y')
 os.system('sudo apt-get install python-pip -y')
+os.system('sudo pip install pip --upgrade')
+os.system('sudo apt-get install python-numpy -y')
 os.system('sudo pip install bottle beautifulsoup')
 os.system('sudo pip install oauth2client')
 os.system('sudo pip install --upgrade google-api-python-client')
@@ -9,5 +11,5 @@ os.system('sudo pip install pyparsing')
 os.system('sudo pip install filedepot')
 os.system('sudo pip install selenium')
 os.system('sudo apt-get install phantomjs -y')
-os.system('ssudo pip install tensorflow --no-cache-dir')
+os.system('sudo pip install tensorflow --no-cache-dir --ignore-installed')
 
